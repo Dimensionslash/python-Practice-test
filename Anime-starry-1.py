17a73c5c1192d36445aca79ea7064217e35e61d3
ReverseBed <55123649+Zack01boy@users.noreply.github.com>
2022-11-11 21:11:37 -0500

    Create Anime-starry-1.py

---
+++ Anime-starry-1.py
@@ -0,0 +1,26 @@
+print ("hello my name is louvensky")
+print ("this will consider day one of me learning python then at the end of my learning I will add more the end\n\n")
+
+print(" OKAY, it is recently 11-9-2022\n\n")
+
+print("............")
+
+print("Juliette")
+print(" Hello my name is Juliette What is your name?")
+
+name = input(":")
+
+
+print(" How was your day, " + name)
+
+input(":")
+print("Juliette")
+print("That's good to know Are you gonna do anything Fun later today.")
+
+Fun = input(":")
+print("Juliette")
+print( Fun + "Sorry I had to pick up this phone call It is my Dad.\n" + "!!Oh my goodness My mother appears to have been in an accident and is currently in the hospital. I must depart. ")
+
+print("... The following next day")
+print("Juliette")
+print(".......")
