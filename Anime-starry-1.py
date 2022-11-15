1571315a148ea3d4c985cd70d081212ed2cf6dcb
ReverseBed <55123649+Zack01boy@users.noreply.github.com>
2022-11-14 21:00:13 -0500

    new

--- Anime-starry-1.py
+++ Anime-starry-1.py
@@ -9,18 +9,29 @@ print("Juliette")
 print(" Hello my name is Juliette What is your name?")
 
 name = input(":")
+if name == "bot":
+    print(" Sorry, but we cannot allow you to enter " + name )
+    exit()
+else:
+    print(" How was your day, " + name)
 
+    input(":")
+    print("Juliette")
+    print("That's good to know Are you gonna do anything Fun later today.")
 
-print(" How was your day, " + name)
+    Fun = input(":")
+    print("Juliette")
+    print( Fun + "\nSorry I had to pick up this phone call It is my Dad.\n\n" + "!!Oh my goodness My mother appears to have been in an accident and is currently in the hospital. I must depart.\n")
 
-input(":")
-print("Juliette")
-print("That's good to know Are you gonna do anything Fun later today.")
-
-Fun = input(":")
-print("Juliette")
-print( Fun + "Sorry I had to pick up this phone call It is my Dad.\n" + "!!Oh my goodness My mother appears to have been in an accident and is currently in the hospital. I must depart. ")
-
-print("... The following next day")
-print("Juliette")
-print(".......")
+    print("... The following next day")
+    print("Juliette")
+    print(".......")
+    
+    
+    
+    
+    
+    
+    
+    
+    
